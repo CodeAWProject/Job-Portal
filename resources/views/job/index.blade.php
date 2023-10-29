98fa9b31988b2ec2a69156f74378a80d90b454da
CodeAWProject <activityware@gmail.com>
2023-10-29 16:47:19 +0100

    Creating a card component and displaying it in the job controller, as well as adding merged attributes to the job controller2

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
     @foreach ($jobs as $job)
-        <div>{{ $job->title }}</div>
+    {{-- Custom class only for this component --}}
+        <x-card class="mb-4">
+            {{ $job->title }}
+        </x-card>
+        <div>Test</div>
     @endforeach
 </x-layout>
