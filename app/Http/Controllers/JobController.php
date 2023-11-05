1e4a54f087248bbe6c94ad9726edf670521cfc5e
CodeAWProject <activityware@gmail.com>
2023-11-05 23:05:55 +0100

    Implementing the display of related employers for a single job

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -49,7 +49,9 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        return view('job.show', ['job' => $job->load('employer')]);
+        return view(
+            'job.show', 
+            ['job' => $job->load('employer.jobs')]);
     }
 
     /**
