4639c6b5e57779a5a194c92caf370f80386f15f9
CodeAWProject <activityware@gmail.com>
2023-11-05 21:32:19 +0100

    Defining the tables in employer migration

---
+++ database/migrations/2023_11_05_201707_create_employers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->id();
+            
+            //Every user can own a company and can add some job offers
+            $table->string('company_name');
+            $table->foreignIdFor(\App\Models\User::class)->constrained();
+            $table->timestamps();
+        });
+
+        //Every job needs to have an Id off the employer
+        Schema::table('jobs', function(Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Employer::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jobs', function(Blueprint $table) {
+            $table->dropForeignIdFor(\App\Models\Employer::class);
+        });
+        
+        Schema::dropIfExists('employers');
+    }
+};
