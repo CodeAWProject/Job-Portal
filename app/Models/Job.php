b0c04f2b7bbe2cd94cdd81a4352317882946db19
CodeAWProject <activityware@gmail.com>
2023-11-06 16:14:55 +0100

    Adding an 'hasUserApplied' action to the job model to check if the current user has already applied to the viewed job; this prevents applying for the same job again

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
@@ -29,6 +30,20 @@ class Job extends Model
     public function jobApplications(): HasMany
     {
         return $this->hasMany(JobApplication::class);
+    }
+
+    public function hasUserApplied(Authenticatable|User|int $user): bool
+    {
+
+        //Checking that the ID of the job is the current model ID.
+        // Getting the job with the ID of this current job model
+        //And then checking if it has a job applications relationship where one of the job applications
+        //has a specific user ID
+        return $this->where('id', $this->id)
+            ->whereHas(
+                'jobApplications',
+                fn($query) => $query->where('user_id', '=', $user->id ?? $user)
+            )->exists();
     }
 
     public function scopeFilter(Builder|QueryBuilder $query, array $filters):Builder|QueryBuilder
