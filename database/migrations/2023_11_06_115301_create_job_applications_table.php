30dcbeae31374867dc7701d5e34da92e8a6cd577
CodeAWProject <activityware@gmail.com>
2023-11-06 12:59:07 +0100

    Defining tables in the job application migration file

---
+++ database/migrations/2023_11_06_115301_create_job_applications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+
+            // You can't have a job application without both a user and a job.
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(\App\Models\Job::class)->constrained;
+            $table->timestamps();
+
+            $table->unsignedInteger('expected_salary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
