aa92fc0bb193d252802bec637e4cc8497c827da0
CodeAWProject <activityware@gmail.com>
2023-11-06 17:26:27 +0100

    Creating a view for MyJobApplication and adding an action to return all applications of the current user

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -9,7 +9,15 @@ class MyJobApplicationController extends Controller
 {
     public function index()
     {
-        //
+        return view('my_job_application.index',
+        [
+            //When you call the relationship method on a model, you get those job applications
+            //scoped to that very user
+            'applications' => auth()->user()->jobApplications()
+                ->with('job', 'job.employer')
+                ->latest()->get()
+        ]
+    );
     }
 
     public function destroy(string $id)
