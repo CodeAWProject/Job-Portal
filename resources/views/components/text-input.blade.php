769a328ff9da2abf3c468aced9b3c43a949f45e9
CodeAWProject <activityware@gmail.com>
2023-11-07 11:59:07 +0100

    Adding conditional classes to the text input component

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -9,7 +9,11 @@
     </button>
     @endif
     <input x-ref="input-{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}" name="{{ $name }}" id="{{$name}}" value="{{ $value }}"
-    class="pr-8 w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2"/>
+    @class(['w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+            'pr-8' => $formRef,
+            'ring-slate-300' => !$errors->has($name),
+            'ring-red-300' => $errors->has($name),
+    ])/>
 
     {{-- The '$message' variable only exists within the error directive --}}
     @error($name)
