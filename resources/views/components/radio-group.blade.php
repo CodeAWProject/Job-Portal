7cd02c2dcb99e40394fcb3b80dc607de95d8888a
CodeAWProject <activityware@gmail.com>
2023-11-07 15:44:44 +0100

    I'm creating a 'create MyJobs' view and implementing the form for it. Additionally, I'm adding the option to disable choices in the radio group component

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,10 +1,11 @@
 <div>
-    <label for="{{  $name  }}" class="mb-1 flex items-center">
-        <input type="radio" name="{{  $name  }}" value="" 
-            @checked(!request($name))/>
-        <span class="ml-2">All</span>
-    </label>
-
+    @if($allOption)
+        <label for="{{  $name  }}" class="mb-1 flex items-center">
+            <input type="radio" name="{{  $name  }}" value="" 
+                @checked(!request($name))/>
+            <span class="ml-2">All</span>
+        </label>
+    @endif
     @foreach ($optionsWithLabels as $label => $option)
         <label for="{{ $name }}" class="mb-1 flex items-center">
             <input type="radio" name="{{ $name }}" value="{{ $option }}" 
